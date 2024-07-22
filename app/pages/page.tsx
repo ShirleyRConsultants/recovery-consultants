c846d576c5ba611052087d0b5261ab5b798b5aa3
Jordon Marchesano <jordonomarchesano@gmail.com>
2024-07-21 21:25:05 -0700

    Questions Start

---
+++ app/pages/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface pageProps {
+    
+}
+
+const page: React.FC<pageProps> = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default page;
