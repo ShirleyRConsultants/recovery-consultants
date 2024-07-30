fe22c4156ed076f0547211746582b025405c8b1c
Jordon Marchesano <jordonomarchesano@gmail.com>
2024-07-29 21:13:34 -0700

    clients page started

--- app/client-pages/Clients.tsx
+++ app/client-pages/Clients.tsx
@@ -1,24 +1,33 @@
-"use client"
-import React from 'react';
-import { createClient } from '@/utils/supabase/client';
-
-interface ClientsProps {
-    
-}
+"use client";
+import React, { useEffect } from "react";
+import { createClient } from "@/utils/supabase/client";
+import { useAuth } from "@/components/Auth";
+import { useRouter } from "next/navigation";
+interface ClientsProps {}
 
 const Clients: React.FC<ClientsProps> = () => {
-
-    const supabase = createClient()
-
-    const handleNew = () =>{
-
+  const router = useRouter();
+  const { profile, session } = useAuth();
+  
+  useEffect(() => {
+    if (
+      profile?.type_of_user !== "admin" &&
+      profile?.type_of_user !== "case_manager"
+    ) {
+      router.push("/");
     }
+  }, [profile]);
+  const supabase = createClient();
 
-    return (
-        <div className=''>
-            <button className="hover:underline " onClick={handleNew}>Add New</button>
-        </div>
-    );
+  const handleNew = () => {};
+
+  return (
+    <div className="">
+      <button className="hover:underline " onClick={handleNew}>
+        Add New
+      </button>
+    </div>
+  );
 };
 
 export default Clients;
