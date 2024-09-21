f53046a5dde1150e9c30fb5a8fcf5abe971b7b45
Jordon Marchesano <jordonomarchesano@gmail.com>
2024-09-21 09:07:19 -0700

    capitalize first letter of first name settings

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,27 +1,35 @@
-"use client"
-import React from 'react';
-import Link from 'next/link';
-import Wave from '@/components/Wave';
-import { useAuth } from '@/components/Auth';
-import NavBar from '@/components/NavBar';
+"use client";
+import React from "react";
+import Link from "next/link";
+import Wave from "@/components/Wave";
+import { useAuth } from "@/components/Auth";
+import NavBar from "@/components/NavBar";
 
-interface SettingsProps {
-    
-}
+interface SettingsProps {}
 
 const Settings: React.FC<SettingsProps> = () => {
-    const {profile} = useAuth()
+  const { profile } = useAuth();
 
-    return (
-        <div className='text-center text-white'>
-            <NavBar/>
-            <div className='bg-mint'>
-           <p className='text-2xl py-10'>Hello {profile?.first_name}!</p>
-            <Link className='hover:font-bold'  href="/reset-password">Reset Password</Link>
-            </div>
-            <Wave/>
-        </div>
-    );
+  return (
+    <div className="text-center text-white">
+      <NavBar />
+      <div className="bg-mint">
+        <p className="text-2xl py-10">
+          Hello{" "}
+          {profile?.first_name &&
+            `${profile.first_name.charAt(0).toUpperCase()}${profile.first_name
+              .slice(1)
+              .toLowerCase()}`}
+          !
+        </p>
+
+        <Link className="hover:font-bold" href="/reset-password">
+          Reset Password
+        </Link>
+      </div>
+      <Wave />
+    </div>
+  );
 };
 
 export default Settings;
